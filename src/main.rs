a7e9709b4c626a63e16cd39c76dd2395465b8ba1
Toon Spin <toon.spin@gmail.com>
2019-09-29 13:27:28 +0200

    Add option to change output field delimiter character

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+mod log_parser;
+
 use std::io::Write;
 
 use std::io::BufRead;
 use std::io::BufWriter;
 
-mod log_parser;
+use structopt::StructOpt;
+
+/// This program reads Apache log data from standard input, processes it, and
+/// writes it to standard output.
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// A character to insert between each output field. [default: tab]
+    #[structopt(value_name="CHAR", short="d", long="delimiter")]
+    field_delimiter: Option<char>,
+}
+
 
 fn main() -> std::io::Result<()> {
     let stdout = std::io::stdout();
     let mut stdout = BufWriter::new(stdout.lock());
+
+    let opt = Opt::from_args();
+
 
     let format_combined = vec![
         log_parser::ParserElement::Word,
@@ -22,13 +37,14 @@ fn main() -> std::io::Result<()> {
     ];
 
     let parser = log_parser::LineParser::new(&format_combined);
+    let delimiter = opt.field_delimiter.unwrap_or('\t') as u8;
 
     for line in std::io::stdin().lock().lines() {
         let line = line.unwrap();
         if let Ok(v) = parser.parse_line(&line) {
             stdout.write(v[0].as_bytes())?;
             for i in 1..v.len() {
-                stdout.write(&[b'\t'])?;
+                stdout.write(&[delimiter])?;
                 stdout.write(v[i].as_bytes())?;
             }
             stdout.write(&[b'\n'])?;
